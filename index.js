ce00d6849ef9c55c9ca604bead75bda76a277666
John <johntaykimani@gmail.com>
2023-12-12 10:13:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const multiply = num1 * num2;
 
 const random = Math.ceil(0.1);
 
-const num3 = 19
-const num4 = 5
+const num3 = 19;
+const num4 = 5;
 
-const mod = num3 % num4
+const mod = num3 % num4;
 
 const max = Math.max(1,5,2, 14, 20, 19, 5, 0, 9);
